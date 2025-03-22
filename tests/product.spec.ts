d30a275fbdd89cabe2370145eb2ce64b3032c6b0
Eduardosntr <alu0101565590@ull.edu.es>
2025-03-22 23:42:26 +0000

    Fix

---
+++ tests/product.spec.ts
@@ -0,0 +1,14 @@
+// import { describe, test, it } from 'vitest';
+// import { Product } from '../src/product';
+
+// describe('Product', () => {
+//   test('should return the correct price', () => {
+//     // const product = new Product('Apple', 2.99);
+//     expect(product.price).toBe(2.99);
+//   });
+
+//   it('should return the correct name', () => {
+//     // const product = new Product('Apple', 2.99);
+//     expect(product.name).toBe('Apple');
+//   });
+// });
